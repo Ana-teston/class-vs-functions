92e16e21dd187f21cbe4dcd0bf3612f6dc9c93ca
Ana-teston <anapaulateston@gmail.com>
2022-08-07 11:15:48 +0200

    remaining components and destructuring

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,20 +1,12 @@
-import { Component } from "react";
-
 import Card from "../card/card.component";
 import './card-list.styles.css';
 
-class CardList extends Component {
-    render() {
-        const { users } = this.props;
-
-        return (
-            <div className='card-list'>
-                {users.map((user) => {
-                    return <Card user={user}/>
-                })}
-            </div>
-        );
-    }
-}
+const CardList = ({users}) => (
+    <div className='card-list'>
+        {users.map((user) => {
+            return <Card user={user}/>
+        })}
+    </div>
+)
 
 export default CardList;
