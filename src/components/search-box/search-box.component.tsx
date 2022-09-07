3d18d1c9f92366200f12ca164a8f1cd17ad2eb86
Ana-teston <anapaulateston@gmail.com>
2022-09-07 16:18:26 +0200

    ChangeEventHandler from react in the search component TSX

--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -1,7 +1,13 @@
-
+import { ChangeEventHandler} from "react";
 import './search-box.styles.css';
 
-const SearchBox = ({ className, placeholder, onChangeHandler }) => (
+type SearchBoxProps = {
+    className: string;
+    placeholder?: string;
+    onChangeHandler: ChangeEventHandler<HTMLInputElement>
+}
+
+const SearchBox = ({ className, placeholder, onChangeHandler }: SearchBoxProps) => (
         <input className={`search-box ${className}`}
                type='search'
                placeholder={placeholder}
