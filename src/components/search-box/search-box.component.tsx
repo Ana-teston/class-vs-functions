d713c8af24882cdee25d82d3f39017efb551f57b
Ana-teston <anapaulateston@gmail.com>
2022-09-07 16:30:13 +0200

    another way for use a function when you to additional parameters

--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -1,10 +1,10 @@
-import { ChangeEventHandler} from "react";
+import { ChangeEvent } from "react";
 import './search-box.styles.css';
 
 type SearchBoxProps = {
     className: string;
     placeholder?: string;
-    onChangeHandler: ChangeEventHandler<HTMLInputElement>
+    onChangeHandler: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
 const SearchBox = ({ className, placeholder, onChangeHandler }: SearchBoxProps) => (
